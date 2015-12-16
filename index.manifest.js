f468523ffca9790c45040323099d34d39310d747
petrakritzinger <petra.kritzinger@gmail.com>
2015-12-16 13:58:54 +0200

    add new files

--- index.manifest.js
+++ index.manifest.js
@@ -14,12 +14,12 @@ var $asm09 = JSIL.GetAssembly("JSIL.Meta, Version=1.0.0.0, Culture=neutral, Publ
 
 if (typeof (contentManifest) !== "object") { JSIL.GlobalNamespace.contentManifest = {}; };
 contentManifest["index"] = [
-    ["Script", "SampleCalculatorApp1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 77746 }],
-    ["Script", "CSharpXamlForHtml5, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 1532787 }],
-    ["Script", "mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089.js", { "sizeBytes": 704862 }],
-    ["Script", "CSharpXamlForHtml5.System.Xaml.dll, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 8386 }],
-    ["Script", "CSharpXamlForHtml5.ToBeReplacedAtRuntime.System.ServiceModel.dll, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 29009 }],
-    ["Script", "CSharpXamlForHtml5.System.dll, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 166324 }],
-    ["Script", "CSharpXamlForHtml5.System.ServiceModel.dll, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 42570 }],
-    ["Script", "CSharpXamlForHtml5.System.Xml.dll, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null.js", { "sizeBytes": 108648 }],
+    ["Script", "SampleCalculatorApp1.js", { "sizeBytes": 77746 }],
+    ["Script", "CSharpXamlForHtml5.js", { "sizeBytes": 1532787 }],
+    ["Script", "mscorlib.js", { "sizeBytes": 704862 }],
+    ["Script", "CSharpXamlForHtml5.System.Xaml.dll.js", { "sizeBytes": 8386 }],
+    ["Script", "CSharpXamlForHtml5.ToBeReplacedAtRuntime.System.ServiceModel.dll.js", { "sizeBytes": 29009 }],
+    ["Script", "CSharpXamlForHtml5.System.dll.js", { "sizeBytes": 166324 }],
+    ["Script", "CSharpXamlForHtml5.System.ServiceModel.dll.js", { "sizeBytes": 42570 }],
+    ["Script", "CSharpXamlForHtml5.System.Xml.dll.js", { "sizeBytes": 108648 }],
 ];
